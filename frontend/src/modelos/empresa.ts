777f8d2919357a7f4f588172be9f61fcd568134f
Cauan Almeida <cauan.almeida@fatec.sp.gov.br>
2023-11-17 14:14:38 -0300

    Criando metódos de adição, edição e delete para serviços e produtos

--- frontend/src/modelos/empresa.ts
+++ frontend/src/modelos/empresa.ts
@@ -34,7 +34,41 @@ export default class Empresa {
     public get getProdutos() {
         return this._produtos
     }
+    public addProdutos(produto: Produto) {
+        this._produtos.push(produto)
+    }
+    public deletarProdutos(index: number){
+        if (index >= 0 && index < this._produtos.length) {
+            this._produtos.splice(index, 1)
+        } else {
+            alert ("Indice inválido")
+        }
+    }
+    public atualizarProdutos(index: number, produto: Produto){
+        if (index >= 0 && index < this._produtos.length) {
+            this._produtos[index] = produto
+        } else {
+            alert ("Indice inválido")
+        }
+    }
     public get getServicos() {
         return this._servicos
+    }
+    public addServicos(servico: Servico) {
+        this._servicos.push(servico)
+    }
+    public deletarServicos(index: number){
+        if (index >= 0 && index < this._servicos.length) {
+            this._servicos.splice(index, 1)
+        } else {
+            alert ("Indice inválido")
+        }
+    }
+    public atualizarServicos(index: number, servico: Servico){
+        if (index >= 0 && index < this._servicos.length) {
+            this._servicos[index] = servico
+        } else {
+            alert ("Indice inválido")
+        }
     }
 }
